007bb751b1a40dc7d467a722733d1feb050e3de0
Никита Ботов <botov164@gmail.com>
2023-05-28 19:46:43 +0300

    Add project files.

---
+++ WINAPI/main.cpp
@@ -0,0 +1,25 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//MessageBox(NULL, "Hello", "Info", MB_YESNO | MB_ICONINFORMATION);
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:break;
+	case WM_COMMAND: break;
+	case WM_CLOSE: EndDialog(hwnd, 0);
+
+	}
+	return FALSE;
+}
